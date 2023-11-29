dad9f9eb1c31843dc41b70b4ae29f401391e2c14
mayur <mayurk@utterwise.tech>
2023-11-29 20:18:54 +0530

    configuration with Tailwind css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="text-lg bg-green-400 text-red-600" >This is a netflix  gpt</div>
   );
 }
 
