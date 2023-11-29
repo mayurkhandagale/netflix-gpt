6ff3551f83bb44f4c05a7d428fd92a064d826428
mayur <mayurk@utterwise.tech>
2023-11-29 20:48:44 +0530

    Header implementation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Body from "./components/Body";
 
 function App() {
   return (
-    <div className="text-lg bg-green-400 text-red-600" >This is a netflix  gpt</div>
+    <Body />
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header';
+
+const Login = () => {
+  return (
+    <div className='bg-gradient-to-b from-black'>
+      <Header />
+      <div>
+        <img src='https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/67033404-2df8-42e0-a5a0-4c8288b4da2c/IN-en-20231120-popsignuptwoweeks-perspective_alpha_website_large.jpg'
+          alt='background-image' />
+      </div>
+    </div>
+  )
+}
+
+export default Login;
